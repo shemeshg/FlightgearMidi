6e1cb1e290aa02a56eca8d703ff9366d88231da9
shemeshg <gili.shemesh@gmail.com>
2026-06-25 00:30:45 +0300

    pybind with midi

--- program-changer/MyLibPy/MyLibPy.cpp
+++ program-changer/MyLibPy/MyLibPy.cpp
@@ -1,16 +1,18 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include "MidiClientUtil.h"
 
 namespace py = pybind11;
 
-int add(int i, int j) {
-    return i + j;
+void test_midi() {
+    auto midiItf = getMidiClientItf();
+    midiItf->testMidi();
 }
 
 PYBIND11_MODULE(MyLibPy, m) {
     m.doc() = "pybind11 MyLibPy plugin";
 
-    m.def("add", &add,
+    m.def("test_midi", &test_midi,
           py::call_guard<py::gil_scoped_release>(),
-          "A function that adds two numbers");
+          "test midi");
 }
