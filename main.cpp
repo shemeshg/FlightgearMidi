fe96ae66b21f27cffe4b58d4504325ba514d7029
shemeshg <gili.shemesh@gmail.com>
2026-06-27 21:38:12 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -2,46 +2,6 @@
 #include "MidiClientUtil.h"
 #include <iostream>
 
-int testTelnet()
-{
-
-    std::unordered_map<std::string, bool> profiles = {
-        {"c172p-fg1000-kap", false}};
-
-    std::string host = "localhost";
-    std::string port = "5500";
-
-    TelnetClient client;
-
-    if (!client.openSocket(host, port))
-    {
-        return 0;
-    }
-
-    // Give FlightGear a brief window to parse and transition to data mode internally
-    std::this_thread::sleep_for(std::chrono::milliseconds(250));
-
-    /*
-    for (float i=0;i<=100;i++){
-        std::cout << "\n[get RESULT] "<< std::to_string(0.01* i)<< " " << client.getValue("/controls/engines/engine[0]/throttle") << "\n\n";
-    }
-
-
-    for (float i = 0; i <= 100; i++)
-    {
-        client.setValue("/controls/engines/engine[0]/throttle", std::to_string(0.01 * i));
-        std::cout << "\n[set RESULT] " << std::to_string(0.01 * i) << "\n\n";
-    }
-    */
-
-    std::string userInput;
-
-    std::getline(std::cin, userInput);
-
-    client.stop();
-
-    return 0;
-}
 
 int main(int argc, char *argv[])
 {
