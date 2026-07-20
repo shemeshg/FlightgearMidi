8bcffecce1d75df255df04c70d87be4d632915a9
shemeshg <gili.shemesh@gmail.com>
2026-07-20 13:28:38 +0300

    commit

--- testPy/LaunchControlXL.py
+++ testPy/LaunchControlXL.py
@@ -25,6 +25,7 @@ class LaunchControlXL:
     previous_air_speed_color: Optional[int] = None
     toggle_states: dict = field(default_factory=dict)
 
+
     # -------------------------------------------------------
     # CONSTANTS (class-level)
     # -------------------------------------------------------
@@ -111,90 +112,86 @@ class LaunchControlXL:
         self.midi_out.sendNoteOn(0, self.FLAPS_LED_ID, color)
 
 
-# ---------------------------------------------------------------------------
-# CONFIG LOADING
-# ---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
+    # CONFIG LOADING
+    # ---------------------------------------------------------------------------
+
+    def loadConfigData(self, cfg: DataConfig, midiClientItf):
+
+
+
+        self.midi = midiClientItf
+
+        midi_input = FlightgearMidi.DataConfigMidiInput()
+        midi_input.midiInputIdx = 0
+        midi_input.midiInputName = "FlightgearOut"
+
+        logger.info("Available MIDI input ports:\n%s",
+                    "\n".join(" " + p for p in self.midi.getInPorts()))
+        logger.info("Available MIDI output ports:\n%s",
+                    "\n".join(" " + p for p in self.midi.getOutPorts()))
+
+        if not self.midi.openLibreMidiOutPort("FlightgearIn", 0):
+            logger.error("Failed to open MIDI output port.")
+            sys.exit(1)
+
+        self.midi_out = self.midi.getLibreMidiOutPort("FlightgearIn", 0)
+
+        self.midi_out.sendNoteOn(0, self.FLAPS_LED_ID, self.COLOR["off"])
+        self.midi_out.sendNoteOn(0, self.AIR_SPEED_LED_ID, self.COLOR["off"])
 
 
 
 
+        # -------------------------------------------------------
+        # CONFIGURATION (kept here exactly as you wanted)
+        # -------------------------------------------------------
 
-def loadConfigData(cfg: DataConfig, midiClientItf):
+        mappings = [
+            (0, 127, 0, 1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 77,
+            "/controls/engines/engine[0]/throttle"),
+            (0, 127, 1, -1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 78,
+            "/controls/flight/rudder"),
+            (0, 127, 1, -1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 79,
+            "/controls/flight/aileron"),
+            (0, 127, -1, 1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 80,
+            "/controls/flight/elevator"),
+            (0, 127, 0, 1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 84,
+            "/controls/engines/current-engine/mixture"),
+        ]
 
+        toggle_mappings = [
+            (FlightgearMidi.MidiMsgType.NOTE_ON, self.CARB_HEAT_LED_ID,
+            "/controls/engines/current-engine/carb-heat"),
+            (FlightgearMidi.MidiMsgType.NOTE_ON, self.LANDING_LIGHTS_LED_ID,
+            "/controls/lighting/landing-lights"),
+            (FlightgearMidi.MidiMsgType.NOTE_ON, self.TAXI_LIGHT_LED_ID,
+            "/controls/lighting/taxi-light"),
+        ]
 
-    ctrl = LaunchControlXL()
-    ctrl.midi = midiClientItf
+        puller_mappings = [
+            ("/controls/flight/flaps", self.FLAPS_LED_ID, self.flaps_on),
+            ("/instrumentation/airspeed-indicator/indicated-speed-kt",
+            self.AIR_SPEED_LED_ID, self.pull_indicated_air_speed),
+        ]
 
-    midi_input = FlightgearMidi.DataConfigMidiInput()
-    midi_input.midiInputIdx = 0
-    midi_input.midiInputName = "FlightgearOut"
+        # -------------------------------------------------------
+        # APPLY BINDINGS (extracted)
+        # -------------------------------------------------------
 
-    logger.info("Available MIDI input ports:\n%s",
-                "\n".join(" " + p for p in ctrl.midi.getInPorts()))
-    logger.info("Available MIDI output ports:\n%s",
-                "\n".join(" " + p for p in ctrl.midi.getOutPorts()))
+        apply_midi_bindings(
+            cfg,
+            midi_input,
+            self.on_off_toggle,
+            self.pull_on_off,
+            mappings,
+            toggle_mappings,
+            puller_mappings
+        )
 
-    if not ctrl.midi.openLibreMidiOutPort("FlightgearIn", 0):
-        logger.error("Failed to open MIDI output port.")
-        sys.exit(1)
-
-    ctrl.midi_out = ctrl.midi.getLibreMidiOutPort("FlightgearIn", 0)
-
-    ctrl.midi_out.sendNoteOn(0, ctrl.FLAPS_LED_ID, ctrl.COLOR["off"])
-    ctrl.midi_out.sendNoteOn(0, ctrl.AIR_SPEED_LED_ID, ctrl.COLOR["off"])
-
-
-
-
-    # -------------------------------------------------------
-    # CONFIGURATION (kept here exactly as you wanted)
-    # -------------------------------------------------------
-
-    mappings = [
-        (0, 127, 0, 1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 77,
-         "/controls/engines/engine[0]/throttle"),
-        (0, 127, 1, -1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 78,
-         "/controls/flight/rudder"),
-        (0, 127, 1, -1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 79,
-         "/controls/flight/aileron"),
-        (0, 127, -1, 1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 80,
-         "/controls/flight/elevator"),
-        (0, 127, 0, 1, FlightgearMidi.MidiMsgType.CONTROL_CHANGE, -1, 84,
-         "/controls/engines/current-engine/mixture"),
-    ]
-
-    toggle_mappings = [
-        (FlightgearMidi.MidiMsgType.NOTE_ON, ctrl.CARB_HEAT_LED_ID,
-         "/controls/engines/current-engine/carb-heat"),
-        (FlightgearMidi.MidiMsgType.NOTE_ON, ctrl.LANDING_LIGHTS_LED_ID,
-         "/controls/lighting/landing-lights"),
-        (FlightgearMidi.MidiMsgType.NOTE_ON, ctrl.TAXI_LIGHT_LED_ID,
-         "/controls/lighting/taxi-light"),
-    ]
-
-    puller_mappings = [
-        ("/controls/flight/flaps", ctrl.FLAPS_LED_ID, ctrl.flaps_on),
-        ("/instrumentation/airspeed-indicator/indicated-speed-kt",
-         ctrl.AIR_SPEED_LED_ID, ctrl.pull_indicated_air_speed),
-    ]
-
-    # -------------------------------------------------------
-    # APPLY BINDINGS (extracted)
-    # -------------------------------------------------------
-
-    apply_midi_bindings(
-        cfg,
-        midi_input,
-        ctrl.on_off_toggle,
-        ctrl.pull_on_off,
-        mappings,
-        toggle_mappings,
-        puller_mappings
-    )
-
-    cfg.dataConfigMidiInputs.append(midi_input)
-    ctrl.midi.setDataConfig(cfg)
-    
+        cfg.dataConfigMidiInputs.append(midi_input)
+        self.midi.setDataConfig(cfg)
+        
 
 
 
@@ -211,7 +208,8 @@ if __name__ == "__main__":
     midiClientItf = FlightgearMidi.getMidiClientItf()
     midiClientItf.pullerSleepInterval = 100
 
-    loadConfigData(cfg, midiClientItf)
+    launchControlXL = LaunchControlXL()
+    launchControlXL.loadConfigData(cfg, midiClientItf)
     
 
 
